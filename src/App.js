add9bc39037f50299cb0933b087e08cef346949a
rfcniblon <51241155+rfcniblon@users.noreply.github.com>
2022-11-19 20:30:36 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,31 +2,25 @@ import React from 'react';
 import './App.css';
 import { Connector } from 'mqtt-react';
 import _MessageContainer from './MessageContainer.js';
-import {subscribe} from 'mqtt-react';
+import { subscribe } from 'mqtt-react';
+require('dotenv').config()
 
-const MessageContainer = subscribe({topic: 'test'})(_MessageContainer);
-//const SERVER_MQTT = process.env.SERVER_ADDRESS_MQTT;
-//const PORT_MQTT = process.env.SERVER_MQTT_PORT;
-//const PORT_WS= process.env.SERVER_WS_PORT;
+const MessageContainer = subscribe({ topic: 'test' })(_MessageContainer);
+const SERVER_MQTT = process.env.REACT_APP_SERVER_MQTT_IP;
+//const PORT_MQTT =process.env.REACT_APP_SERVER_MQTT_PORT ;
+const PORT_WS = process.env.REACT_APP_SERVER_WS_PORT;
 
-const SERVER_MQTT = "192.168.1.62";
-const PORT_WS= 8885;
-
-
-function App(){
-  
-    return (
-      <Connector mqttProps={"ws://"+SERVER_MQTT +":"+PORT_WS+"/"} >
-         {/* <Connector mqttProps="ws://192.168.1.62:8885/"> */}
+function App() {
+  return (
+    <Connector mqttProps={"ws://" + SERVER_MQTT + ":" + PORT_WS + "/"} >
       <div className="offset-sm-3 col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 mt-2 bgg">
         <div className="offset-sm-3 col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 mt-2">
           <h2 className='text-center pt-3 pb-3'>MQTT React</h2>
         </div>
-        <MessageContainer/> 
+        <MessageContainer />
       </div>
-      </Connector>
-    );
-  }
-
+    </Connector>
+  );
+}
 
 export default App;
