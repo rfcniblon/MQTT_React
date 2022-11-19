32b42d8df35363fd7e24b4f08f64cf46dc43a1bb
rfcniblon <51241155+rfcniblon@users.noreply.github.com>
2022-11-19 20:42:07 +0100

    Update MessageSonde.js

--- src/MessageSonde.js
+++ src/MessageSonde.js
@@ -1,42 +1,47 @@
 import React from 'react';
 
- const MessageSonde=({data}) => {
-  
-  const Result =()=>{
-    if(data && data[0]){
-        const temp = data[0].svalue;
-        const temperature = temp.substring(0,5);
-        const humidite = temp.substring(6,11);
-     return(
-      <div>
+const MessageSonde = ({ data }) => {
+  const Result = () => {
+    if (data[0] && data[0]) {
+      const temp = data[0].svalue;
+      const temperature = temp.substring(0, 5);
+      const humidite = temp.substring(6, 11);
+      return (
+        <div>
           <section className=''>
             <div className='d-flex'>
-                <p>Id sonde Température :</p><span className='ms-1'>{data[0] ? data[0].idx: null}</span> 
+              <p>Id sonde 1 :</p><span className='ms-1'>{data[0] ? data[0].idx : null}</span>
             </div>
             <div className='d-flex'>
-              <p>Température :</p><span className='ms-1'>{data[0] ? temperature:null}C°</span>
+              <p>Température :</p><span className='ms-1'>{data[0] ? temperature : null}C°</span>
             </div>
             <div className='d-flex'>
-              <p>Humidité :</p><span className='ms-1'>{data[0] ? humidite:null}%</span>
+              <p>Humidité :</p><span className='ms-1'>{data[0] ? humidite : null}%</span>
             </div>
-         </section>
-         <section className=''>
-           <div className='d-flex'>
-              <p>Id sonde Sol :</p><span className='ms-1'>{data[1] ? data[1].idx: null}</span>  
-              </div>
-              <div className='d-flex'>
-              <p>Taux humidité :</p><span className='ms-1'>{data[1] ? data[1].svalue:null} %</span>
-              </div>
-           </section>
-     </div>
-     )
+          </section>
+          <section className=''>
+            <div className='d-flex'>
+              <p>Id sonde 2 :</p><span className='ms-1'>{data[1] ? data[1].idx : null}</span>
+            </div>
+            <div className='d-flex'>
+              <p>Humidité du sol :</p><span className='ms-1'>{data[1] ? data[1].svalue : null} %</span>
+            </div>
+          </section>
+        </div>
+      )
+    } else {
+      return (
+        <div className='text-center'>
+          <p>Aucune données reçu de l'esp 8266</p>
+        </div>
+      )
     }
   }
-
+  
   return (
     <div className='pb-3'>
-      <h3 className='text-center'>Réception des messages</h3>
-     {Result()}
+      <h3 className='text-center'>Réception des messages ESP8266</h3>
+      {Result()}
     </div>
   )
 }
