12eebbabf57a137e4bb77877182cc6b894f0d1f5
rfcniblon <51241155+rfcniblon@users.noreply.github.com>
2022-11-19 18:45:33 +0100

    Add files via upload

---
+++ src/MessageSonde.js
@@ -0,0 +1,67 @@
+import React from 'react';
+
+ const MessageSonde=({data}) => {
+  
+  const Result =()=>{
+    if(data && data[0]){
+        const temp = data[0].svalue;
+        const temperature = temp.substring(0,5);
+        const humidite = temp.substring(6,11);
+     return(
+      <div>
+          <section className=''>
+            <div className='d-flex'>
+                <p>Id sonde Température :</p><span className='ms-1'>{data[0] ? data[0].idx: null}</span> 
+            </div>
+            <div className='d-flex'>
+              <p>Température :</p><span className='ms-1'>{data[0] ? temperature:null}C°</span>
+            </div>
+            <div className='d-flex'>
+              <p>Humidité :</p><span className='ms-1'>{data[0] ? humidite:null}%</span>
+            </div>
+         </section>
+         <section className=''>
+           <div className='d-flex'>
+              <p>Id sonde Sol :</p><span className='ms-1'>{data[1] ? data[1].idx: null}</span>  
+              </div>
+              <div className='d-flex'>
+              <p>Taux humidité :</p><span className='ms-1'>{data[1] ? data[1].svalue:null} %</span>
+              </div>
+           </section>
+     </div>
+     )
+    }
+  }
+
+
+  return (
+    <div className='pb-3'>
+      <h3 className='text-center'>Réception des messages</h3>
+     {Result()}
+    </div>
+  )
+}
+
+export default MessageSonde
+
+
+
+
+// import React from 'react';
+
+// export default ({data}) => {
+  
+//   const dataList = data.map((d, i) => <li key={i}className="list-group-item mt-1">{d}</li>)
+  
+//   console.log(data);
+//   console.log(data && data[0]);
+//   return (
+//     <div className='pb-3'>
+//       <h3 className='text-center'>Réception des messages</h3>
+//       <ul className='list-group'>
+//         {dataList}
+//       </ul>
+
+//     </div>
+//   )
+// }
