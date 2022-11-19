4d19cfe75bea33143db66cc2f228bba4d75901ff
rfcniblon <51241155+rfcniblon@users.noreply.github.com>
2022-11-19 20:41:06 +0100

    Update MessageContainer.js

--- src/MessageContainer.js
+++ src/MessageContainer.js
@@ -1,27 +1,35 @@
-import React from 'react';
-//import MessageList from './MessageList';
+import React, { useState } from 'react';
+import MessageList from './MessageList';
 import MessageForm from './MessageForm';
 import MessageSonde from './MessageSonde';
 
 const MessageContainer = (props) => {
+  const [isShow, setIsShow] = useState(false);
 
-  const addMessage=(message)=>{
-    const {mqtt} = props;
+  const addMessage = (message) => {
+    const { mqtt } = props;
+    // Ici on publie dans le canal "test"
     mqtt.publish('test', message);
   }
 
-    return (
-      <div className='container-fluid'>
- <div className='offset-sm-1  col-10 col-sm-10 col-md-8 col-lg-8 col-xl-8'>
-        <MessageForm onSubmit={addMessage} />
-        </div>
-        <div className='offset-sm-1 col-10 col-sm-11 col-md-10 col-lg-10 col-xl-10 pt-2'>
-      <MessageSonde data={props.data}/> 
-       {/* <MessageList data={props.data} />  */}
-        </div>
-      </div>
-    )
-
+  const Show = () => {
+    setIsShow(!isShow)
   }
 
-export default  MessageContainer
+  return (
+    <div className='container-fluid'>
+      <div className='d-flex justify-content-center offset-sm-2  col-10 col-sm-10 col-md-8 col-lg-8 col-xl-8'>
+      <button className='btn btn-success' type="submit" onClick={Show}>{!isShow ? "Lecture des données":" Lecture donnée esp8266"}</button>
+      </div>
+      <div className='offset-sm-2  col-10 col-sm-10 col-md-8 col-lg-8 col-xl-8'>
+        <MessageForm onSubmit={addMessage} />
+      </div>
+      <div className='offset-sm-1 col-10 col-sm-11 col-md-10 col-lg-10 col-xl-10 pt-2'>
+        {isShow ?
+          <MessageSonde data={props.data} /> :
+          <MessageList data={props.data} />}
+      </div>
+    </div>
+  )
+}
+export default MessageContainer
