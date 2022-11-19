4c526f935789f7a85fb7baee9ea02f09ad23a104
rfcniblon <51241155+rfcniblon@users.noreply.github.com>
2022-11-19 20:42:51 +0100

    Update MessageList.js

--- src/MessageList.js
+++ src/MessageList.js
@@ -1,13 +1,19 @@
 import React from 'react';
 
-export default ({data}) => {
-  const dataList = data.map((d,idx) => <li key={idx} className="list-group-item mt-1">{d}</li>)
+ const MessageList=({data}) => {
+  //console.log(data);
+
   return (
     <div className='pb-3'>
       <h3 className='text-center'>Réception des messages</h3>
       <ul className='list-group'>
-        {dataList}
+       {data && data.map((d) => 
+        <li className="list-group-item mt-1" key={d}>
+          {d}
+        </li>
+        )} 
       </ul>
     </div>
   )
 }
+export default MessageList
